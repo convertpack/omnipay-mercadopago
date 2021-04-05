4576bef94b5b642d3dba4a57dfcabd8aa54d61ac
Hugo Maestá <hmaesta@gmail.com>
2021-04-05 11:59:15 -0300

    feat(purchase-response): convert boleto barcode to common format

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -44,15 +44,18 @@ class PurchaseResponse extends AbstractResponse implements RedirectResponseInter
         $boletoBarCode = null;
         $boletoURL = null;
 
+        // If payment method is 'Boleto'
         if ($data['payment_type_id'] == 'ticket') {
             $dateOfExpiration = date('Y-m-d', strtotime($data['date_of_expiration'])) . 'T22:00:00-0300';
 
-            $boletoBarCode = Arr::get($data, 'barcode.content');
+            $rawBarCode = Arr::get($data, 'barcode.content'); 
+            $boletoBarCode = $this->convertItfBoleto($rawBarCode);
+
             $boletoURL = Arr::get($data, 'transaction_details.external_resource_url');
         }
 
         return [
-            'id' => (string) $data['id'],
+            'provider_id' => (string) $data['id'],
             'installments' => (int) $data['installments'],
             'installments_fee' => $this->getFeeByKey('financing_fee'), // only MP - installments fee
             'gateway_fee' => $this->getFeeByKey('mercadopago_fee'), // only MP - processing fee
@@ -124,4 +127,66 @@ class PurchaseResponse extends AbstractResponse implements RedirectResponseInter
             return $this->getRequest()->getTestMode() ? $this->data->sandbox_init_point : $this->data->init_point;
         }
     }
+
+    /*
+     * Mercado Pago returns Boleto's barcode
+     * in ITF format and must be converted
+     * to the most common format
+     */
+    function convertItfBoleto($code)
+    {
+        $code = preg_replace('/\D/', '', $code);
+
+        $field1 = substr($code, 0, 4) . substr($code, 19, 1) . '.' . substr($code, 20, 4);
+        $field2 = substr($code, 24, 5) . '.' . substr($code, 24 + 5, 5);
+        $field3 = substr($code, 34, 5) . '.' . substr($code, 34 + 5, 5);
+        $field4 = substr($code, 4, 1); // "Dígito verificador"
+        $field5 = substr($code, 5, 14); // Due date + amount
+
+        if ($field5 === 0) {
+            $field5 = '000';
+        }
+
+        $newCode = $field1 . modulo10($field1) 
+            . $field2 . modulo10($field2)
+            . $field3 . modulo10($field3)
+            . $field4
+            . $field5;
+
+        return $newCode;
+    }
+
+    function modulo10($number)
+    {
+        $numero = preg_replace('/\D/', '', $number);
+
+        $soma = 0;
+        $peso = 2;
+
+        $contador = strlen($numero) - 1;
+
+        while ($contador >= 0) {
+            $multiplicacao = (substr($numero, $contador, 1) * $peso);
+            if ($multiplicacao >= 10) {
+                $multiplicacao = 1 + ($multiplicacao - 10);
+            }
+            $soma = $soma + $multiplicacao;
+
+            if ($peso === 2) {
+                $peso = 1;
+            } else {
+                $peso = 2;
+            }
+            $contador = $contador - 1;
+        }
+
+        $digito = 10 - ($soma % 10);
+
+        if ($digito === 10) {
+            $digito = 0;
+        }
+
+        return $digito;
+    }
+
 }
