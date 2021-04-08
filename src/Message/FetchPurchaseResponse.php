b57ca0bfe9eba0683ede3de462a04e17f8549241
Emanuel Pedro <emanueltotem@gmail.com>
2021-04-08 15:38:42 -0300

    fix(fetch_purchase): Added methods to get status and status_detail of transaction

--- src/Message/FetchPurchaseResponse.php
+++ src/Message/FetchPurchaseResponse.php
@@ -2,7 +2,17 @@
 
 namespace Omnipay\MercadoPago\Message;
 
+use Illuminate\Support\Arr;
+
 class FetchPurchaseResponse extends AbstractResponse
 {
+    public function getTransactionStatus()
+    {
+        return Arr::get($this->data, 'status');
+    }
 
+    public function getTransactionStatusDetail()
+    {
+        return Arr::get($this->data, 'status_detail');
+    }
 }
